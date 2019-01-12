a41286847e7a6d13025b1d9c6d8ad89c301402ad
kartickvad <24665612+kartickvad@users.noreply.github.com>
2019-01-12 15:45:45 +0530

    Update for 2018-19, and print in-hand pay, not the difference between employee and consultant's tax.
    ... since people care about take-home pay.
    
    - Print info for one salary, not a range.
    - Mention python3, not python3.6, in the hashbang, since we're already at 3.7.
    - Use the unit of thousands consistently.
    - Cess increases from 3 to 4%.
    - If surcharge is applicable, prints a warning instead of silently calculating it inaccurately.
    - Round take-home pay down rather than to the nearest, to be conservative.
    - Add link to info on tax rates for 2018-19 fiscal (2019-20 assessment year).
    - Take care of the ₹2500 limit on income tax for low-income earners.
    
    I validated this with an online tax calculator, and both say that a consultant who earns 10 lac takes ₹82K home each month.

--- emp.py
+++ emp.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3.6
-# Run this in Python 3.6.
+#!/usr/bin/env python3
+# Run this in Python 3.
 
 # Author: Vaddadi Kartick
 # Released under Apache license 2.0.
@@ -12,21 +12,24 @@
 #     script.
 #  1) You're paid a certain amount of money, and you have to pay all taxes out of it, including GST
 #     -- you can't pass the GST along by grossing it up.
-#  2) Your income is < 50 lac. This script doesn't take surcharges into account.
-#  3) This is for fiscal year 2017-18.
-#  4) You're a resident individual of India, not a company.
-#  5) You're not a senior citizen.
-#  6) This doesn't take into account the rebate of up to ₹2500 for low income earners.
-#  7) All numbers are annual.
-#  8) This script doesn't take into account the cost in your time of dealing with the bureaucratic
+#  2) This is for fiscal year 2018-19.
+#  3) You're a resident individual of India, not a company.
+#  4) You're not a senior citizen.
+#  5) Numbers are annual, and in thousands of rupees, except when stated otherwise.
+#  6) This script doesn't take into account the cost in your time of dealing with the bureaucratic
 #     GST regime, or the cost of hiring a CA to do so.
-#  9) GST paid can't be set off against something else.
+#  7) GST paid can't be set off against something else.
+
+# Reference: https://www.bankbazaar.com/tax/income-tax-slabs.html
+
+
+import math
 
 PRESUMPTIVE_RATE = .5
-# Your presumptive taxation applies at this rate. Change it if you fall under a different slab.
-# Read up on presumptive taxation at https://cleartax.in/s/sugam-itr-4s-form
+# Consultants benefit from presumptive taxation  at this rate. Change it if you fall under a
+# different slab. Read up on presumptive taxation at https://cleartax.in/s/sugam-itr-4s-form
 
-EMPLOYEE_TAX_DEDUCTION = 160 * 1000
+EMPLOYEE_TAX_DEDUCTION = 160
 # Deductions you can claim if you’re an employee, but not if you're a consultant. This includes
 # section 80C, 80D medical insurance, and so on. Change it as suitable.
 
@@ -35,13 +38,9 @@ GST_RATE = .18
 # nor the entity paying you is registered under GST — set this to 0. Or if you're exporting, which
 # is zero-rated, set this to 0.
 
-EFFECTIVE_GST_RATE = GST_RATE/(1 + GST_RATE)
-# If you earn ₹100 on which you need to pay GST from your own pocket, you pay around ₹15, not ₹18.
-# Don't change this formula.
-
-SLAB_1 = 250 * 1000
-SLAB_2 = 500 * 1000
-SLAB_3 = 1000 * 1000
+SLAB_1 = 250
+SLAB_2 = 500
+SLAB_3 = 1000
 
 # How much income tax is due under slab 1?
 def income_tax_slab_1(income):
@@ -50,7 +49,7 @@ def income_tax_slab_1(income):
   if income > SLAB_2:
     income = SLAB_2
   income -= SLAB_1
-  return income * .05
+  return income * .05  # 5%
 
 # How much income tax is due under slab 2?
 def income_tax_slab_2(income):
@@ -59,19 +58,23 @@ def income_tax_slab_2(income):
   if income > SLAB_3:
     income = SLAB_3
   income -= SLAB_2
-  return income * .2
+  return income * .2  # 20%
 
 # How much income tax is due under slab 3?
 def income_tax_slab_3(income):
   if income <= SLAB_3:
     return 0
   income -= SLAB_3
-  return income * .3
+  return income * .3  #30%
 
 # How much income tax is due?
 def incomeTaxFor(income):
   tax = income_tax_slab_1(income) + income_tax_slab_2(income) + income_tax_slab_3(income)
-  return tax * 1.03  # Cess
+  if income <= 350:
+    tax = min(tax, 2.5)  # If you earn < 3.5 lac, your tax is limited to ₹2500.
+  if income >= 50 * 100:
+    print("Warning: ignoring surcharge for high income")
+  return tax * 1.04  # Health and education cess
 
 # How much tax does an employee pay for the given income?
 def taxForEmployee(income):
@@ -79,19 +82,24 @@ def taxForEmployee(income):
 
 # How much tax does a consultant pay for the given income?
 def taxForConsultant(income):
+  EFFECTIVE_GST_RATE = GST_RATE/(1 + GST_RATE)
+  # If you earn ₹100 on which you need to pay GST from your own pocket, you pay around ₹15, not ₹18.
+  # Don't change this formula.
+
   gst = income * EFFECTIVE_GST_RATE
   income -= gst  # Income tax is net of GST.
   income *= PRESUMPTIVE_RATE
   return incomeTaxFor(income) + gst
 
-print("The first column is your income (in ₹ lac) , and the second is the extra tax paid by an employee over a consultant (in ₹ thousand):")
+def monthlyInHandForEmployee(income):
+  return math.floor((income - taxForEmployee(income)) / 12)
 
-# i is income in lacs. Stop before 50, because a surcharge applies at 50.
-for i in range(1, 50):
-  income = i * 100 * 1000
-  employee_tax = taxForEmployee(income)
-  consultant_tax = taxForConsultant(income)
-  extra = employee_tax - consultant_tax
-  extra /= 1000  # Thousands.
-  extra = round(extra)
-  print(f"{i}, {extra}")
+def monthlyInHandForConsultant(income):
+  return math.floor((income - taxForConsultant(income)) / 12)
+
+def calculateAndPrint(income):
+  in_hand_employee = monthlyInHandForEmployee(income)
+  in_hand_consultant = monthlyInHandForConsultant(income)
+  print(f"An employee earns ₹{in_hand_employee}K, while a consultant earns ₹{in_hand_consultant}K, in hand each month.")
+
+calculateAndPrint(10 * 100)
