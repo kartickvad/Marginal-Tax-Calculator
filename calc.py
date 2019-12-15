9c245ba512f4ace0b0f0c443b5a54a15bbda755f
Vaddadi Kartick <24665612+kartickvad@users.noreply.github.com>
2019-12-15 12:41:34 +0530

    Add comments to make it understadable to someone who doesn't know tax well

--- calc.py
+++ calc.py
@@ -5,6 +5,13 @@
 # Author: Vaddadi Kartick
 # Released under Apache license 2.0.
 
+
+# At a high level, there are three taxes:
+#   - Income tax
+#   - Professional tax
+#   - GST (not for employees)
+
+
 # This script calculates take-home pay for a given CTC, for both an employee and a consultant.
 #
 # Assumptions:
@@ -18,33 +25,54 @@
 #     GST regime, or the cost of hiring a CA to do so.
 #  7) GST paid can't be set off against something else.
 #  8) HRA and LTA are ignored.
+#  9) Professional tax is calculated for Karnataka.
 #
 # Reference: https://www.bankbazaar.com/tax/income-tax-slabs.html
+
+# All values are in thousands.
 
 
 import math
 
-# PF is required to be paid only by companies with 10 or more employees.
+# If your company has 10 or more employees, it's required to contribute to the
+# Employees' Provident Fund (EPF), at the rate of 25.61%, in which case you should set this variable
+# to 0.2561.
 #
-# 25.61% of CTC goes to PF and pension. See
+# See
 # https://www.bankbazaar.com/saving-schemes/guide-to-understanding-the-employee-pension-scheme.html
 PF_RATE = 0
 
+# Professionals pay an extra state, which varies from state to state. In Karnataka, it's ₹200 per
+# month, except for one month, where it's ₹300. This applies to both employees and consultants.
 PROFESSIONAL_TAX = 2.5
 
+# Some consultants benefit from presumptive taxation at this rate. This means that 50% of your
+# income is tax-free, and only the remaining 50% is considered taxable income.
+#
+# But for some people, only 8% of income is considered taxable, in wihch case you should set this
+# to 0.08. Others pay 6%.
+#
+# Read up on presumptive taxation at https://cleartax.in/s/sugam-itr-4s-form. It falls under section
+# 44AD and 44ADA.
 PRESUMPTIVE_RATE = .5
-# Consultants benefit from presumptive taxation  at this rate. Change it if you fall under a
-# different slab. Read up on presumptive taxation at https://cleartax.in/s/sugam-itr-4s-form
 
-EMPLOYEE_TAX_DEDUCTION = 160
 # Deductions you can claim if you’re an employee, but not if you're a consultant. This includes
-# section 80C, 80D medical insurance, and so on. Change it as suitable.
+# section 80C (up to 1.5 lac), 80D medical insurance, and so on. Change it as suitable.
+# See https://cleartax.in/s/income-tax-savings
+# Instead of having separate values for each section, we've simplified our work by asking the user
+# to total them up and enter the total here:
+EMPLOYEE_TAX_DEDUCTION = 160
 
 GST_RATE = 0
 # What GST rate do you fall under? Change it as suitable. If you don't pay GST at all — neither you
 # nor the entity paying you is registered under GST — set this to 0. Or if you're exporting, which
 # is zero-rated, set this to 0.
+#
+# Even if you're paying GST, you should typically leave this at zero, because if you agree to offer
+# a service at ₹100, you would raise an invoice for more than this amount, thus passing on the GST
+# burden to your client.
 
+# The tax slabs are 2.5 lac, 5 lac and 10 lac:
 SLAB_1 = 250
 SLAB_2 = 500
 SLAB_3 = 1000
@@ -55,11 +83,11 @@ def lac(amount):
 # How much income tax is due under slab 1?
 def income_tax_slab_1(income):
   if income <= SLAB_1:
-    return 0
+    return 0  # No tax under this slab, since your income is too low.
   if income > SLAB_2:
-    income = SLAB_2
-  income -= SLAB_1
-  return income * .05  # 5%
+    income = SLAB_2  # Income that falls under the next slab shouldn't be taxed in this one.
+  income -= SLAB_1 # Only the part of your income that exceeds the limit is taxed.
+  return income * .05  # 5% tax rate for this slab
 
 # How much income tax is due under slab 2?
 def income_tax_slab_2(income):
@@ -79,10 +107,12 @@ def income_tax_slab_3(income):
 
 # How much income tax is due?
 def income_tax_for(income):
+  # People with income less than 5 lac pay no tax, though according to the slabs, they're required
+  # to:
   if income <= 500:
     return 0
   tax = income_tax_slab_1(income) + income_tax_slab_2(income) + income_tax_slab_3(income)
-  cess = tax * .04  # Health and education cess.
+  cess = tax * .04  # Health and education cess is 4% of the tax.
   surcharge = 0
   if income >= lac(100):
     print("Warning: ignoring surcharge for high income")
@@ -103,10 +133,14 @@ def total_tax_for(income, is_employee):
   # Don't change this formula.
 
   gst = income * EFFECTIVE_GST_RATE
-  income -= gst  # Income tax is net of GST.
+  
+  # Income tax is net of GST. In other words, income tax is applicable only on the remaining income after paying GST:
+  income -= gst
+
   income *= PRESUMPTIVE_RATE
   return income_and_professional_tax_for(income) + gst
 
+# Calculates PF (and pension, which we don't need to worry about separately):
 def pf_for(income):
   # To calculate PF, your salary is capped at 15K.
   return min(income, 15) * PF_RATE
@@ -115,7 +149,7 @@ def take_home(income, is_employee):
   tax = total_tax_for(income, is_employee)
   pf = pf_for(income) if is_employee else 0
   income = income - tax - pf
-  return math.floor(income / 12)
+  return math.floor(income / 12)  # Round to the nearest thousand.
   
 def format_money(amount):
   if amount >= 100:
