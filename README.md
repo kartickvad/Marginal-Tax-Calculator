9b114fceea7b255867dba50843b0b9a58b1e1ae3
Kartick Vaddadi <kartick.vad@gmail.com>
2021-05-28 19:24:25 +0530

    Remove GST
    ... since it doesn't apply to foreign companies, and since clients pay it.

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
-**This project is no longer updated. Get the latest version for**:
-  * [Mac](https://apps.apple.com/in/app/india-tax-calculator/id1492061607?mt=12)
-  * [iPhone](https://apps.apple.com/in/app/reverse-tax-calculator-india/id1491759482)
-
-
-
 This script determines:
-  - When you're made an offer for a given CTC, how much will you take home every month?
-  - If you want a certain amount of take-home pay every month, what CTC should you ask for?
+
+  * When you're made an offer for a given CTC, how much will you take home every month?
+  * If you want a certain amount of take-home pay every month, what CTC should you ask for?
 
 This is for Indians.
 
 This script handles:
-  - Differing tax treatments for employees and consultants. The latter are eligible for presumptive taxation under section 44AD.
-  - Health and education cess
-  - Surcharge
-  - GST
-  - PF and pension
-  - Section 80C investments
-  - Professional tax.
+
+  * Differing tax treatments for employees and consultants. The latter are eligible for presumptive taxation under section 44AD.
+  * Health and education cess
+  * Surcharge
+  * PF and pension
+  * Section 80C investments
+  ** Professional tax.
 
 The script makes some assumptions, listed at the top of the file. Verify that those apply to you, taking your CA's help if needed. Some parameters can be changed; again, see the comments.
 
--- calc.py
+++ calc.py
@@ -2,30 +2,27 @@
 # coding: utf-8
 # Run this in the latest version of Python. I tested with 3.7.
 
-# Author: Vaddadi Kartick
+# Author: Kartick Vaddadi
 # Released under Apache license 2.0.
 
 
-# At a high level, there are three taxes:
+# This script handles
 #   - Income tax
 #   - Professional tax
-#   - GST (not for employees)
 
 
 # This script calculates take-home pay for a given CTC, for both an employee and a consultant.
 #
 # Assumptions:
-#  1) GST comes out of your pocket — you can't pass the GST along by grossing it up.
-#  2) This is for fiscal year 2019-20.
-#  3) You're a resident individual of India, not a company.
-#  4) You're not a senior citizen.
-#  5) Numbers are annual, and in thousands of rupees, except when stated otherwise. Take-home pay
+#  - This is for fiscal year 2019-20.
+#  - You're a resident individual of India, not a company.
+#  - You're not a senior citizen.
+#  - Numbers are annual, and in thousands of rupees, except when stated otherwise. Take-home pay
 #     is always monthly.
-#  6) This script doesn't take into account the cost in your time of dealing with the bureaucratic
-#     GST regime, or the cost of hiring a CA to do so.
-#  7) GST paid can't be set off against something else.
-#  8) HRA and LTA are ignored.
-#  9) Professional tax is calculated for Karnataka.
+#  - This script doesn't take into account the cost in your time of dealing with the bureaucracy
+#     regime, or the cost of hiring a CA to do so.
+#  - HRA and LTA are ignored.
+#  - Professional tax is calculated for Karnataka.
 #
 # Reference: https://www.bankbazaar.com/tax/income-tax-slabs.html
 
@@ -57,27 +54,18 @@ PROFESSIONAL_TAX = 2.5
 PRESUMPTIVE_RATE = .5
 
 # Deductions you can claim if you’re an employee, but not if you're a consultant. This includes
-# section 80C (up to 1.5 lac), 80D medical insurance, and so on. Change it as suitable.
+# section 80C (up to 1.5 lakh), 80D medical insurance, and so on. Change it as suitable.
 # See https://cleartax.in/s/income-tax-savings
 # Instead of having separate values for each section, we've simplified our work by asking the user
 # to total them up and enter the total here:
 EMPLOYEE_TAX_DEDUCTION = 160
 
-GST_RATE = 0
-# What GST rate do you fall under? Change it as suitable. If you don't pay GST at all — neither you
-# nor the entity paying you is registered under GST — set this to 0. Or if you're exporting, which
-# is zero-rated, set this to 0.
-#
-# Even if you're paying GST, you should typically leave this at zero, because if you agree to offer
-# a service at ₹100, you would raise an invoice for more than this amount, thus passing on the GST
-# burden to your client.
-
-# The tax slabs are 2.5 lac, 5 lac and 10 lac:
+# The tax slabs are 2.5, 5 and 10 lakh:
 SLAB_1 = 250
 SLAB_2 = 500
 SLAB_3 = 1000
 
-def lac(amount):
+def lakh(amount):
   return amount * 100
 
 # How much income tax is due under slab 1?
@@ -107,38 +95,29 @@ def income_tax_slab_3(income):
 
 # How much income tax is due?
 def income_tax_for(income):
-  # People with income less than 5 lac pay no tax, though according to the slabs, they're required
+  # People with income less than 5 lakh pay no tax, though according to the slabs, they're required
   # to:
   if income <= 500:
     return 0
   tax = income_tax_slab_1(income) + income_tax_slab_2(income) + income_tax_slab_3(income)
   cess = tax * .04  # Health and education cess is 4% of the tax.
   surcharge = 0
-  if income >= lac(100):
+  if income >= lakh(100):
     print("Warning: ignoring surcharge for high income")
-  if income >= lac(50):
+  if income >= lakh(50):
     surcharge = tax * .1  # 10% of tax
   return tax + cess + surcharge
 
 def income_and_professional_tax_for(income):
   return income_tax_for(income) + PROFESSIONAL_TAX
 
-# Comprises of income tax, professional tax and GST.
+# Comprises of income tax and professional tax.
 def total_tax_for(income, is_employee):
   if is_employee:
     return income_and_professional_tax_for(income - EMPLOYEE_TAX_DEDUCTION) 
 
-  EFFECTIVE_GST_RATE = GST_RATE/(1 + GST_RATE)
-  # If you earn ₹100 on which you need to pay GST from your own pocket, you pay around ₹15, not ₹18.
-  # Don't change this formula.
-
-  gst = income * EFFECTIVE_GST_RATE
-  
-  # Income tax is net of GST. In other words, income tax is applicable only on the remaining income after paying GST:
-  income -= gst
-
   income *= PRESUMPTIVE_RATE
-  return income_and_professional_tax_for(income) + gst
+  return income_and_professional_tax_for(income)
 
 # Calculates PF (and pension, which we don't need to worry about separately):
 def pf_for(income):
@@ -154,7 +133,7 @@ def take_home(income, is_employee):
 def format_money(amount):
   if amount >= 100:
     amount /= 100
-    return f"{amount} lac"
+    return f"{amount} lakh"
   return f"{amount}K"
 
 def print_take_home_for(income):
@@ -173,6 +152,6 @@ def print_ctc_for_take_home_pay(desired_take_home):
   ctc_consultant = format_money(ctc_for_take_home_pay(desired_take_home, is_employee = False))
   print(f"To take home {format_money(desired_take_home)} a month, an employee should ask for a CTC of {ctc_employee}, while a consultant should ask for a CTC of {ctc_consultant}.")
     
-print_take_home_for(lac(10))
+print_take_home_for(lakh(10))
 print()
 print_ctc_for_take_home_pay(50)
